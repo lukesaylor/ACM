362cc0270d9d517ec51e0ab783f047871b2399b9
lukesaylor <slappyhoursound@gmail.com>
2020-11-02 12:31:59 -0500

    initial commit

---
+++ ACM.BL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Class1
+    {
+    }
+}
