4d6ddd8152fb3d0d365dc3fffc980d4cc19345bd
lukesaylor <slappyhoursound@gmail.com>
2020-11-02 15:44:17 -0500

    added validation

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -18,9 +18,20 @@ namespace ACM.BL
         {
             get
             {
-                return LastName + ", " + FirstName;
+                string fullName = LastName;
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(fullName))
+                    {
+                        fullName += ", ";
+                    }
+                    fullName += FirstName;
+                }
+                return fullName;
             }
         }
+
+        public static int InstanceCount { get; set; }
 
 
         private string _lastName;
@@ -34,6 +45,16 @@ namespace ACM.BL
             {
                 _lastName = value;
             }
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(LastName)) isValid = false;
+            if (string.IsNullOrWhiteSpace(EmailAddress)) isValid = false;
+
+            return isValid;
         }
     }
 }
--- Tests/ACM.BLTest/CustomerTest.cs
+++ Tests/ACM.BLTest/CustomerTest.cs
@@ -45,13 +45,37 @@ namespace ACM.BLTest
             Customer customer = new Customer
             {
                 FirstName = "Lukas",
-                 
+
             };
             string expected = "Lukas";
             //act
             string actual = customer.FullName;
             // assert
             Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void StaticTest()
+        {
+            //arrange
+            var c1 = new Customer();
+            c1.FirstName = "Bilbo";
+            Customer.InstanceCount += 1;
+
+            var c2 = new Customer();
+            c2.FirstName = "Frodo";
+            Customer.InstanceCount += 1;
+
+            var c3 = new Customer();
+            c3.FirstName = "Rosie";
+            Customer.InstanceCount += 1;
+
+            //Act
+
+            //assert
+
+            Assert.Equal(3, Customer.InstanceCount);
+
         }
     }
 }
