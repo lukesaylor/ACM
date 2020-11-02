d435b2d201df862cc940df8f99137101eaa8e776
lukesaylor <slappyhoursound@gmail.com>
2020-11-02 14:31:53 -0500

    first test

--- Tests/ACM.BLTest/CustomerTest.cs
+++ Tests/ACM.BLTest/CustomerTest.cs
@@ -1,14 +1,25 @@
 using System;
 using Xunit;
+using ACM.BL;
 
 namespace ACM.BLTest
 {
     public class CustomerTest
     {
         [Fact]
-        public void Test1()
+        public void FullNameTestValid()
         {
-
+            //arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Lukas",
+                LastName = "Saylor"
+            };
+            string expected = "Saylor,Lukas";
+            //act
+            string actual = customer.FullName;
+            // assert
+            Assert.Equal(expected, actual);
         }
     }
 }
