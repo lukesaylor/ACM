9084b1528c7f11444fe7890c160c3a2e754bb632
Lukas Saylor <slappyhoursound@gmail.com>
2020-11-03 17:38:52 -0500

    new methods added

---
+++ ACM.BL/Order.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace ACM.BL
+{
+    public class Order
+    {
+        public Order()
+        {
+        }
+        public Order(int orderId)
+        {
+            OrderId = orderId;
+        }
+
+        public DateTimeOffset? OrderDate { get; set; }
+        public int OrderId { get; private set; }
+
+        //retrieve one order
+        public Order Retrieve(int orderId)
+        {
+            return new Order();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (OrderDate == null) isValid = false;
+
+            return isValid;
+        }
+    }
+}
