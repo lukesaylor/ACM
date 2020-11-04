3f7cd722d58fd3a5c78442e7e2318f7d53586ddf
Lukas Saylor <slappyhoursound@gmail.com>
2020-11-04 16:49:47 -0500

    * CustomerRepository.cs: cutomer repo
    * CustomerRepositoryTest.cs: customer repo test

---
+++ ACM.BL/CustomerRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace ACM.BL
+{
+    public class CustomerRepository
+    {
+        public CustomerRepository()
+        {
+
+        }
+
+        public Customer Retrieve(int customerId)
+        {
+            Customer customer = new Customer(customerId);
+
+            //temp hard code
+
+            if (customerId == 1)
+            {
+                customer.EmailAddress = "fbaggins@hobbiton.me";
+                customer.FirstName = "Frodo";
+                customer.LastName = "Baggins";
+            }
+            return customer;
+        }
+
+        public bool Save(Customer customer)
+        {
+            return true;
+        }
+        
+    }
+}
---
+++ Tests/ACM.BLTest/CustomerRepositoryTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    public class CustomerRepositoryTest
+    {
+        [Fact]
+        public void RetrieveValid()
+        {
+            //arrange
+            var customerRepository = new CustomerRepository();
+            var expected = new Customer(1)
+            {
+                EmailAddress = "fbaggins@hobbiton.me",
+                FirstName = "Frodo",
+                LastName = "Baggins"
+            };
+            //act
+            var actual = customerRepository.Retrieve(1);
+            //assert
+            Assert.Equal(expected, actual);
+            Assert.Equal(expected, actual);
+            Assert.Equal(expected, actual);
+            Assert.Equal(expected, actual);
+
+        }
+
+         
+    }
+}
